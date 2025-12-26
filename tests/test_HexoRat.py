1a6266d99cabcedbf3ea9abec5ee7bababb5a65e
Samyar Sharafi <138717806+Samyar-Sharafi@users.noreply.github.com>
2025-12-26 23:16:02 +0330

    Update test_HexoRat.py

--- tests/test_HexoRat.py
+++ tests/test_HexoRat.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 
 from HexoRat import has_cmp, has_jcc, has_xor, has_backward_jump, score_window
